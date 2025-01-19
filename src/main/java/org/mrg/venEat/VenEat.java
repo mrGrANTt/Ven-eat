e7875312d62958255334781279d4f2f51a2d8fe6
mrGrANTt <sambistyyy0@gmail.com>
2025-01-19 14:04:55 +1000

    Structure created

--- src/main/java/org/mrg/venEat/VenEat.java
+++ src/main/java/org/mrg/venEat/VenEat.java
@@ -1,18 +1,29 @@
 package org.mrg.venEat;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
 import org.bukkit.block.sign.Side;
 import org.bukkit.block.sign.SignSide;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class VenEat extends JavaPlugin implements Listener {
 
@@ -28,10 +39,16 @@ public class VenEat extends JavaPlugin implements Listener {
         //  TODO: Сделать то же но с подтягиванием данных из папки
         //
 
+        // Рецепт Build Tool
+        ShapedRecipe buildTool = new ShapedRecipe(new NamespacedKey(this, "build_tool"), BuildItem());
+        buildTool.shape(" S ", " S ", " S ");
+        buildTool.setIngredient('S', Material.STICK);
+        getServer().addRecipe(buildTool);
+
 
         // Рецепт теста
         ShapedRecipe doughRecipe = new ShapedRecipe(new NamespacedKey(this, "dough"), new ItemStack(Material.BREAD, 1));
-        doughRecipe.shape(" S ", "W ", "W ");
+        doughRecipe.shape(" S ", " W ", " W ");
         doughRecipe.setIngredient('S', Material.SUGAR);
         doughRecipe.setIngredient('W', Material.WHEAT);
         getServer().addRecipe(doughRecipe);
@@ -45,32 +62,90 @@ public class VenEat extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onSignChange(SignChangeEvent event) {
-        Block block = event.getBlock();
-
-        //
-        // TODO: сделать через позицию блока камня а не игрока
-        //
-
-        if (block.getType().name().contains("_SIGN")) {
-            Sign sign = (Sign) block.getState();
-
-            if(checkSide(sign.getSide(Side.FRONT)) || checkSide(sign.getSide(Side.BACK))) {
-                Block below = block.getRelative(BlockFace.DOWN, 2);
-                if (isCustomFurnace(below)) {
-                    below.setType(Material.FURNACE);
-                    below.getWorld().playSound(below.getLocation(), Sound.BLOCK_FURNACE_FIRE_CRACKLE, 1, 1);
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if(event.getClickedBlock() != null) {
+            if (event.getClickedBlock().getType().name().contains("_SIGN")) {
+                if (checkBuildItem(event.getItem())) {
+                    event.setCancelled(true);
+                    check(event.getClickedBlock(), event.getPlayer());
                 }
             }
         }
     }
 
+    private ItemStack BuildItem() {
+        ItemStack item = Material.STICK.asItemType().createItemStack();
+        NamespacedKey key = new NamespacedKey(this, "furnace_build_tool");
+
+        item.editMeta(meta -> {
+            meta.setEnchantmentGlintOverride(true);
+            meta.getPersistentDataContainer().set(key, PersistentDataType.BOOLEAN, true);
+            meta.displayName(Component.text("Furnace build tool"));
+        });
+
+        return  item;
+    }
+
+    private boolean checkBuildItem(ItemStack item) {
+        if(item != null && item.getType() == Material.STICK) {
+            NamespacedKey key = new NamespacedKey(this, "furnace_build_tool");
+            AtomicBoolean has = new AtomicBoolean(false);
+
+            item.editMeta(meta -> {
+                if (meta.getPersistentDataContainer().has(key))
+                    has.set(true);
+            });
+            return has.get();
+        }
+        return false;
+    }
+
+    private void check(Block block, Player plr) {
+        Sign sign = (Sign) block.getState();
+        if(checkSide(sign.getSide(Side.FRONT)) || checkSide(sign.getSide(Side.BACK))) {
+            Block center = getCenter(block);
+            if (center != null && isCustomFurnace(center)) {
+                BuildFurnace(center);
+                block.setType(Material.AIR);
+                center.getWorld().playSound(center.getLocation(), Sound.BLOCK_ANVIL_DESTROY, 10, 1);
+                plr.sendMessage(Component.text("Печь установлена").color(TextColor.color(29, 147, 16)));
+            }
+        }
+    }
+
+    private void BuildFurnace(Block block) {
+        MetadataValue value = new FixedMetadataValue(this, block.getLocation());
+        for (int x = -1; x < 2; x++)
+            for (int y = -1; y < 2; y++)
+                for (int z = -1; z < 2; z++)
+                    furnacingBlock(block.getRelative(x,y,z), value);
+    }
+
+    private void furnacingBlock(Block block, MetadataValue value) {
+        block.setType(Material.FURNACE);
+        block.setMetadata("FurnaceCenter", value);
+    }
+
+    private Block getCenter(Block sign) {
+        BlockFace[] faces = new BlockFace[6];
+        faces[0] = BlockFace.NORTH;
+        faces[1] = BlockFace.WEST;
+        faces[2] = BlockFace.EAST;
+        faces[3] = BlockFace.SOUTH;
+        faces[4] = BlockFace.UP;
+        faces[5] = BlockFace.DOWN;
+
+        for (BlockFace face : faces) {
+            if(sign.getRelative(face).getType() == Material.getMaterial(Objects.requireNonNull(getConfig().getString("furn-material")))) {
+                return sign.getRelative(face, 2);
+            }
+        }
+        return null;
+    }
+
     private boolean checkSide(SignSide side) {
         for(Component line : side.lines()) {
-        /*
-            TODO: Подумать может можно игнорировать регистр как-то
-        */
-            if(line.contains(Component.text("[печь]")) || line.contains(Component.text("[Печь]"))) {
+            if(MiniMessage.miniMessage().serialize(line).toLowerCase().contains("[печь]")) {
                 return true;
             }
         }
@@ -78,22 +153,15 @@ public class VenEat extends JavaPlugin implements Listener {
     }
 
     private boolean isCustomFurnace(Block centerBlock) {
-        if (centerBlock.getType() != Material.AIR) return false;
-        /*
-            TODO: Переделать.. Очень всратое решение
-        */
-        for (int x = -1; x <= 1; x++){
-            for (int y = -1; y <= 1; y++){
-                for (int z = -1; z <= 1; z++) {
-                    if(x == 0 && y == 0 && z == 0) continue;
-                    if (centerBlock
-                            .getRelative(BlockFace.UP, y)
-                            .getRelative(BlockFace.NORTH, z)
-                            .getRelative(BlockFace.EAST, x)
-                            .getType() != Material.COBBLESTONE) return false;
+        for (int x = -1; x < 2; x++)
+            for (int y = -1; y < 2; y++)
+                for (int z = -1; z < 2; z++) {
+                    if (x == 0 && y == 0 && z == 0)
+                        continue;
+                    if (centerBlock.getRelative(x, y, z).getType() != Material.getMaterial(Objects.requireNonNull(getConfig().getString("furn-material")))) {
+                        return false;
+                    }
                 }
-            }
-        }
         return true;
     }
 }
